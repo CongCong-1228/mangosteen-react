9acc9f383eda9e5a113b261ce595058b40b78242
fivecong <fivesun1228@163.com>
2024-01-16 16:12:35 +0800

    新增路由与路由切换动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from "react-router-dom"
+import { animated, useTransition } from "@react-spring/web"
+import { ReactNode, useRef } from "react"
+import { Outlet, useLocation, useOutlet } from "react-router-dom"
 
 
 export const WelcomeLayout: React.FC = () => {
-    return (
-        <>
-            <Outlet />
-        </>
+    const map = useRef<Record<string, ReactNode>>({})
+    const location = useLocation()
+    const outlet = useOutlet()
+    map.current[location.pathname] = outlet
+    const transitions = useTransition(location.pathname, {
+        // 进入状态
+        from: { transform: location.pathname === '/welcome/1/' ? 'translateX(0%)' : 'translateX(100%)', },
+        // 稳定状态
+        enter: { transform: 'translateX(0%)', },
+        // 退出状态
+        leave: { transform: 'translateX(-100%)', },
+        config: { duration: 300 }
+
+    })
+    return transitions((style, pathname) =>
+        <animated.div key={pathname} style={style}>
+            <div style={{ textAlign: 'center' }}>
+                {map.current[pathname]}
+            </div>
+        </animated.div>
     )
 }
+
+
