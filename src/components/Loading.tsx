6c96c1099be2cff8bc0df59ca76f52f11ea8fc2c
mitani <fivesun1228@163.com>
2024-09-24 14:12:39 +0800

    update 封装loading组件 引入 classnames

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,36 @@
-export const Loading: React.FC = () => {
-  return <div>Loading...</div>;
+import styled, { keyframes } from "styled-components";
+import { Icon } from "./Icon";
+import c from "classnames";
+
+interface LoadingProps {
+  className?: string;
+  message?: string;
+}
+
+export const Loading: React.FC<LoadingProps> = ({
+  className,
+  message = "加载中...",
+}) => {
+  const spin = keyframes`
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    `;
+  const Div = styled.div({
+    svg: {
+      animation: `${spin} 1.25s linear infinite`,
+    },
+  });
+
+  return (
+    <Div className={c("flex flex-col justify-center items-center", className)}>
+      <Icon name="loading" className="w-128px h-128px" />
+      <p p-8px text-lg>
+        {message}
+      </p>
+    </Div>
+  );
 };
